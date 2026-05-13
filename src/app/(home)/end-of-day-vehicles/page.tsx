1416cfac004c36dabb55a3d05ae9f603bb6399ce
NguyenTanTai <ngtantai48@gmail.com>
2026-05-13 09:51:19 +0700

    Refactor code structure for improved readability and maintainability

--- src/app/(home)/end-of-day-vehicles/page.tsx
+++
@@ -1,7 +0,0 @@
-import EndOfDayVehicleManager from "@/components/features/end-of-day/EndOfDayVehicleManager";
-
-export default function EndOfDayVehiclesPage() {
-  return (
-    <EndOfDayVehicleManager />
-  );
-}
